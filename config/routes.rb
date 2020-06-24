61b882b46d2bdaa09ca391b66a18c5221de8a727
Uchinomaru <bravecred@gmail.com>
2020-06-24 21:37:22 +0900

    リポジトリ初期化

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get '/', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

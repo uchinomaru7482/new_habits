c9d22407bb55a9aacd930c3c07cff32d863fa831
Uchinomaru <bravecred@gmail.com>
2020-06-27 13:20:50 +0900

    Add:登録フォーム追加

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,4 +1,21 @@
 class HabitsController < ApplicationController
   def new
+  	@habit = Habit.new
+  end
+
+  def create
+  	@habit = Habit.find(params[:id])
+  	if @habit.save
+  		flash[:success] = "習慣を登録しました"
+  		redirect_to "/"
+  	else
+  		render "new"
+  	end
+  end
+
+
+  private 
+  def habit_params
+  	params.require(:habit).permit(:user_id, :habit_content, :habit_type, :total_days, :total_time, :continuation_days, :open_range)
   end
 end

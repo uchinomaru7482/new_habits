67c476741552cdb903d84eee46d418302c603c24
Uchinomaru <bravecred@gmail.com>
2020-07-11 16:38:07 +0900

    Add：投稿時に習慣継続日数をカウントする機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   	@post.user_id = @habit.user_id
   	if @post.user_id == current_user.id && @post.save
       save_achievement
+      count_days
       redirect_to "/"
   	else
   	  render "new"
@@ -36,7 +37,8 @@ class PostsController < ApplicationController
 
   #習慣を今日行ったかどうかを保存
   def save_achievement
-    if params[:post][:check] == "true"
+    puts already_save_achievement?
+    if params[:post][:check] == "true" && already_save_achievement?
       @achievement = Achievement.new
       @achievement.habit_id = params[:habit_id]
       @achievement.check = true
@@ -44,7 +46,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def already_save_achievement?
+    @today = Date.current.all_day
+    @achievement = Achievement.find_by(habit_id: params[:habit_id], created_at: @today)
+    @achievement.nil?
+  end
+
   def count_days
-    
+    @habit = Habit.find(params[:habit_id])
+    @count_value = Achievement.where(habit_id: params[:habit_id]).size
+    @habit.total_days = @count_value
+
+    @yesterday = (Date.current - 1).all_day
+    if Achievement.find_by(habit_id: params[:habit_id], created_at: @yesterday)
+      @habit.continuation_days += 1
+    else
+      @habit.continuation_days = 1
+    end
+    @habit.save
   end
 end

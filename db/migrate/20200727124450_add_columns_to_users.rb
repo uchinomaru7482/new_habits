6035e2357f31c0e076d2d4e999413065876b33b9
Uchinomaru <bravecred@gmail.com>
2020-08-09 10:41:21 +0900

    Fix：userモデルのname,imageカラムの追加位置を修正

--- db/migrate/20200727124450_add_columns_to_users.rb
+++ db/migrate/20200727124450_add_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :name, :string
-    add_column :users, :image, :string
+    add_column :users, :name, :string, after: :email
+    add_column :users, :image, :string, after: :name
   end
 end

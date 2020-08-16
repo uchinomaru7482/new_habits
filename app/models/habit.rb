344f71fcc54b2e92f5ccfc2bb45066f1c37c6502
Uchinomaru <bravecred@gmail.com>
2020-08-16 11:42:54 +0900

    Add：userモデルのテストを作成

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,5 +1,5 @@
 class Habit < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :posts, dependent: :destroy
   has_many :achievements, dependent: :destroy
 

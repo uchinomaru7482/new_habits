7b06c17c105386309aec807d0621e6685e9f24a6
Uchinomaru <bravecred@gmail.com>
2020-08-16 11:44:46 +0900

    Add：habitモデルのテストを追加

---
+++ spec/models/habit_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Habit, type: :model do
+  it "is valid with a content and record_type" do
+  	user = FactoryBot.create(:user)
+  	habit = Habit.new(
+  	  content: "毎日読書をする",
+  	  record_type: false,
+  	  user_id: user.id,
+  	)
+  	expect(habit).to be_valid
+  end
+
+  it "is invalid without a content" do
+  	habit = FactoryBot.build(:habit, content: nil)
+  	habit.valid?
+  	expect(habit.errors[:content]).to include("を入力してください")
+  end
+
+  it "is invalid without a record_type" do
+  	habit = FactoryBot.build(:habit, record_type: nil)
+  	habit.valid?
+  	expect(habit.errors[:record_type]).to include("は一覧にありません")
+  end
+end

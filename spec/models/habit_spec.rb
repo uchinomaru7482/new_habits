d1b3b24d9520037c7e928650b8e69835781fd70c
Uchinomaru <bravecred@gmail.com>
2020-08-18 20:24:53 +0900

    Add：habitモデルのcontent文字数制限のテストを追記

--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -17,6 +17,12 @@ RSpec.describe Habit, type: :model do
   	expect(habit.errors[:content]).to include("を入力してください")
   end
 
+  it "is invalid with a content more than the maximum characters" do
+    habit = FactoryBot.build(:habit, content: "x" * 41)
+    habit.valid?
+    expect(habit.errors[:content]).to include("は40文字以内で入力してください")
+  end
+
   it "is invalid without a record_type" do
   	habit = FactoryBot.build(:habit, record_type: nil)
   	habit.valid?

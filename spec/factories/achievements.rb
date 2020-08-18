851715a6b036de8bfea5d9d85ab9726599690bdd
Uchinomaru <bravecred@gmail.com>
2020-08-18 19:59:23 +0900

    Add：achievementモデルのテストを追加

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :achievement do
-    
+    check true
+    report 1
+    association :habit
   end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a check and report" do
+  	habit = FactoryBot.create(:habit)
+  	achievement = Achievement.new(
+  	  check: true,
+  	  report: 3,
+  	  habit_id: habit.id,
+  	)
+  end
 end

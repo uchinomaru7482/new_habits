b60dd53986baf3dd6f0caa42ef5163752e05fc81
Uchinomaru <bravecred@gmail.com>
2020-08-25 16:35:51 +0900

    Add：habitモデルのクラススペックを追加

--- spec/factories/achievements.rb
+++ spec/factories/achievements.rb
@@ -4,4 +4,13 @@ FactoryBot.define do
     report 1
     association :habit
   end
+
+  trait :created_at_yesterday do
+  	created_at 1.day.ago
+  end
+
+  trait :check_false do
+  	check false
+  	created_at 2.day.ago
+  end
 end
--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -28,4 +28,32 @@ RSpec.describe Habit, type: :model do
   	habit.valid?
   	expect(habit.errors[:record_type]).to include("は一覧にありません")
   end
+
+  it "returns total days" do
+    achievement = FactoryBot.create(:achievement)
+    habit = achievement.habit
+    achievement_yesterday = FactoryBot.create(:achievement, :created_at_yesterday, habit: habit)
+    achievement_check_false = FactoryBot.create(:achievement, :check_false, habit: habit)
+    expect(habit.count_total_days).to eq 2
+  end
+
+  it "returns continuation days" do
+    habit = FactoryBot.create(:habit, continuation_days: 1)
+    achievement_yesterday = FactoryBot.create(:achievement, :created_at_yesterday, habit: habit)
+    achievement = FactoryBot.create(:achievement, habit: habit)
+    expect(habit.count_continuation_days).to eq 2
+  end
+
+  it "return the reset continuation days" do
+    habit = FactoryBot.create(:habit, continuation_days: 3)
+    achievement = FactoryBot.create(:achievement, habit: habit)
+    expect(habit.count_continuation_days).to eq 1
+  end
+
+  it "return the count total time" do
+    habit = FactoryBot.create(:habit)
+    achievement_yesterday = FactoryBot.create(:achievement, :created_at_yesterday, habit: habit)
+    achievement = FactoryBot.create(:achievement, habit: habit)
+    expect(habit.count_total_time).to eq 2
+  end
 end

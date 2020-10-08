9fd3a06202381962327773dd7a065c72d62e3b67
Uchinomaru <bravecred@gmail.com>
2020-10-08 09:15:11 +0900

    Add：メールサーバー設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # デフォルトURLを設定
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+  # メール設定
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => ENV['SMTP_ADDRESS'],
+    :port => ENV['SMTP_PORT'],
+    :user_name => ENV['SMTP_USER_NAME'],
+    :password => ENV['SMTP_PASSWD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end

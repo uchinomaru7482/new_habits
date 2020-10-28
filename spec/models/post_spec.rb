dadaf3f767ca986999a71b20075739659b66149b
Uchinomaru <bravecred@gmail.com>
2020-10-28 09:07:40 +0900

    Fix：postのモデルスペックを修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,12 @@ require "rails_helper"
 RSpec.describe Post, type: :model do
   it "is valid with a content" do
     habit = FactoryBot.create(:habit)
-    Post.new(
+    post = Post.new(
       content: "今日は「具体と抽象」を読んだ",
+      habit_id: habit.id,
       user_id: habit.user_id
     )
+    expect(post).to be_valid
   end
 
   it "is invalid without a content" do

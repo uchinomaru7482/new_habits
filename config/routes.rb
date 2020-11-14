3c69aae383123d964082385bed8a4216a09f7384
Uchinomaru <bravecred@gmail.com>
2020-11-14 10:54:54 +0900

    Improve：deviceのルーティングをハッシュロケットを使わない記述に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => "users/registrations"
-  }
   root to: "static_pages#home"
   get "/lp", to: "static_pages#lp"
   get "/help", to: "static_pages#help"
   get "/terms_of_service", to: "static_pages#terms_of_service"
   get "/privacy_policy", to: "static_pages#privacy_policy"
 
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
   resources :users, only: [:show] do
-  	get "/following", to: "users#following" 
-  	get "/followers", to: "users#followers"
+    get "/following", to: "users#following" 
+    get "/followers", to: "users#followers"
   end
   resources :habits do
     resources :posts, only: [:new, :create]

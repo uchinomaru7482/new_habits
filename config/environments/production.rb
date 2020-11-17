991776a1d0772e9ade63689535714b3ceae57717
uchinomaru7482 <bravecred@gmail.com>
2020-11-17 07:26:47 +0000

    Add：本番環境でメールを送信できるよう設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # デフォルトURLを設定
+  config.action_mailer.default_url_options = {host: "https://www.new-habits.net"}
+  # メール設定
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => ENV['SMTP_ADDRESS'],
+    :port => ENV['SMTP_PORT'],
+    :user_name => ENV['SMTP_USER_NAME'],
+    :password => ENV['SMTP_PASSWD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end

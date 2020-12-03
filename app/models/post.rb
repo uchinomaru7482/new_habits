cc1a9a8cc7bcce86d04a6df1e275906116b3b2f3
Uchinomaru <bravecred@gmail.com>
2020-12-03 10:25:07 +0900

    Add：アソシエーションを記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   mount_uploader :image, PostImageUploader
 
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :users, through: :likes
   belongs_to :user
   belongs_to :habit
 

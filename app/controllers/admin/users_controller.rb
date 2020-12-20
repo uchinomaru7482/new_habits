5e10f3a25faec36cd7be41206205c6dd55219c68
Uchinomaru <bravecred@gmail.com>
2020-12-20 12:39:55 +0900

    管理者のユーザー削除機能を追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+class Admin::UsersController < ApplicationController
+  before_action :admin_user
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    #テストユーザーが削除できるのはサンプルユーザーのみ
+    unless params[:id].to_i.between?(2, 8)
+      flash[:notice] = "サンプルユーザー以外は削除できません"
+      redirect_to admin_users_path and return
+    end
+    User.find(params[:id]).destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def admin_user
+    if current_user.admin == false
+      flash[:notice] = "管理者権限がありません"
+      redirect_to root_path
+    end
+  end
+end

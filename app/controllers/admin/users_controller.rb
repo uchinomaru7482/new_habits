9b71db36bfcf5d0ba592156d8ca67c1441a63820
Uchinomaru <bravecred@gmail.com>
2020-12-20 16:32:04 +0900

    Fix：RuboCopに従いコードを修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
-    #テストユーザーが削除できるのはサンプルユーザーのみ
+    # テストユーザーが削除できるのはサンプルユーザーのみ
     unless params[:id].to_i.between?(2, 8)
       flash[:notice] = "サンプルユーザー以外は削除できません"
       redirect_to admin_users_path and return
@@ -18,9 +18,9 @@ class Admin::UsersController < ApplicationController
   private
 
   def admin_user
-    if current_user.admin == false
-      flash[:notice] = "管理者権限がありません"
-      redirect_to root_path
-    end
+    return unless current_user.admin == false
+
+    flash[:notice] = "管理者権限がありません"
+    redirect_to root_path
   end
 end

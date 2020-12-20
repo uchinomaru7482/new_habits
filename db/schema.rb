c4c8afe9fa3563a77a23280c8d83302823bd6dba
Uchinomaru <bravecred@gmail.com>
2020-12-20 13:03:50 +0900

    Fix：カラム追加位置変更

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_12_20_015141) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "name", null: false
+    t.boolean "admin", default: false, null: false
     t.string "image"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -80,7 +81,6 @@ ActiveRecord::Schema.define(version: 2020_12_20_015141) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

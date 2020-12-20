a8830199e4c66911422a89926d8d25884951e1bf
Uchinomaru <bravecred@gmail.com>
2020-12-20 16:44:32 +0900

    Add：user関連のテストにadminカラムを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user, aliases: [:owner] do
     sequence(:email) { |n| "tester#{n}@example.com" }
     sequence(:name) { |n| "tester#{n}" }
+    admin false
     password "aaaaaaaa"
     password_confirmation "aaaaaaaa"
     confirmed_at Time.current
